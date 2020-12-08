97b8f0c80aad924533d66c51478507dc73060a2a
dhayakardhaya <dhayakar@digitaldots.ai>
2020-12-08 16:08:38 +0530

    Initial commit

---
+++ src/main/java/com/digitaldots/bablyonapi/BablyonapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitaldots.bablyonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BablyonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BablyonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitaldots/bablyonapi/BablyonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaldots.bablyonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BablyonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
